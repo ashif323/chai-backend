2f744ee9b5c1dde21d8e52ab399e5758e6ceb164
ashif323 <ashifiqbal0783@gmail.com>
2024-12-15 21:00:26 +0530

    fix logout function

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -25,7 +25,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentUserPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, UpdateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"))
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"))
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
